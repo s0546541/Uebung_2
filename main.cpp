9efce78307e621933fd71938ced8bab0fb84c7c5
Philipp Lange <philipp.lange23@gmail.com>
2016-04-09 17:00:35 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Philipp
+ *
+ * Created on 9. April 2016, 16:56
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
